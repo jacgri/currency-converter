a703cd61676a5d5a7585074c0e16c878dd6180d4
Jacqui Grimes <grimesjacqui@gmail.com>
2017-07-13 21:14:05 +0100

    adding error message - issues to be fixed

--- js/form.js
+++ js/form.js
@@ -4,11 +4,17 @@ $('#js-convert-gbp').submit(function(event){
     var target = event.target
     var amount = Number(target.amount.value)
     var currency = target.currency.value
-    
     var convertedAmount = currencyConverter.convertTo(currency, amount)
-    $("#convertedRate").html(function(){
-    return(amount + ' GBP in ' + currency + ' is ' + Number(convertedAmount).toFixed(2))
+    
+    if ((amount == true) && (currency == true)){
+        $("#convertedRate").html(function(){
+        return(amount + ' GBP in ' + currency + ' is ' + Number(convertedAmount).toFixed(2))
+        })
+    } else {
+        $("#errorMessage").html(function(){
+        return('Please enter an amount and choose a currency')
+        })
+    }
 
-    })
 })
 })
